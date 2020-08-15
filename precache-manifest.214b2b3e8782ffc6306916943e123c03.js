ce62532984d44343a852ca6111934fe19410bd04
Youngho105 <a93492011@gmail.com>
2020-08-16 08:49:23 +0900

    Updates

---
+++ precache-manifest.214b2b3e8782ffc6306916943e123c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d201eb029d88b2628b8ac07a549373",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "458ed957b4b4547bd0e2",
+    "url": "/movie_app/static/css/main.d7bcd188.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458ed957b4b4547bd0e2",
+    "url": "/movie_app/static/js/main.ce9e5180.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
